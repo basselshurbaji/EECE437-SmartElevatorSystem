a21c43e262ac26f42a3539be51af5ae92193f987
monkeyDLuffyyy <sarahgame9@gmail.com>
2020-12-22 00:23:02 +0200

    floor docs

--- src/main/java/floor/Floor.java
+++ src/main/java/floor/Floor.java
@@ -1,7 +1,6 @@
 package floor;
 
-/*
- * STATUS: In Progress/Used in our system.
+/**
  * This class is used to represent the different floors in the elevator system. 
  * Every floor has a unique id and can be assigned a specific priority.
  */
@@ -9,21 +8,32 @@ public class Floor {
 	private int id;
 	private FloorPriority priority;
 	
-	//Constructor
+	/**
+	 * Public constructor of floor.
+	 * It initiates the floor's id and priority.
+	 */
 	public Floor(int id, FloorPriority priority) {
 		this.id = id;
 		this.priority = priority;
 	}
 	
-	//Getters & Setters
+	/**
+	 * @return returns the floor's priority.
+	 */
 	public FloorPriority getPriority() {
 		return priority;
 	}
 
+	/**
+	 * Sets the floor's priority.
+	 */
 	public void setPriority(FloorPriority priority) {
 		this.priority = priority;
 	}
 
+	/**
+	 * @return returns the floor's id.
+	 */
 	public int getId() {
 		return id;
 	}
