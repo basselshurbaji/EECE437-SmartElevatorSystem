09fec4525e05e8266eaf07378d7b0e668bb62111
Bassel Shurbaji <basselshurbaji@gmail.com>
2020-12-21 21:14:05 +0200

    fixed issue

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"elevatorManager","l":"ElevatorManager"},{"p":"elevatorManager","l":"ElevatorManagerDestinationRequest"},{"p":"elevatorManager","l":"ElevatorManagerMode"},{"p":"elevatorManager","l":"ElevatorManagerRequest"}]
