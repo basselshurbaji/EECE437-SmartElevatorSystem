a13dd9ce27db5058024bf058cc18487494a5cb91
monkeyDLuffyyy <sarahgame9@gmail.com>
2020-12-21 23:48:53 +0200

    test

--- src/main/java/userInteraction/UserPriority.java
+++ src/main/java/userInteraction/UserPriority.java
@@ -1,9 +1,6 @@
 package userInteraction;
 
-/*
- * STATUS: In Progress/Not used in current implementation
- * Enum UserPriority is used to indicate the user priority.
- */
+
 public enum UserPriority {
 	NORMAL,
 	HIGH
