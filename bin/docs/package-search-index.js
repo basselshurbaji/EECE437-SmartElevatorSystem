c9e2c24078d84118db1e8c6119c9ca18636592d4
hsandid <hadisandid@gmail.com>
2020-12-22 10:27:08 +0200

    Added comments

---
+++ bin/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dataMonitor"},{"l":"demo"},{"l":"elevator"},{"l":"elevatorManager"},{"l":"floor"},{"l":"GUI"},{"l":"userInteraction"}];updateSearchResults();
